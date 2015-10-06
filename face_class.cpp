d2a88cc31859b1521745f430f80bd33f2a112440
Paul Kourany <pkourany@users.noreply.github.com>
2015-10-06 13:34:18 -0400

    Add clock face classes

---
+++ face_class.cpp
@@ -0,0 +1,12 @@
+#include "face_class.h"
+
+
+//FaceClass::FaceClass() { }
+//FaceClass::~FaceClass() { }
+
+//void FaceClass::begin(void) { }				// Reset face for running
+
+//void FaceClass::run(void) { }					// Update the face animation
+
+//int FaceClass::isComplete(void) {  return -1;}	// True if animation completed, false if not, -1 if not applicable
+
